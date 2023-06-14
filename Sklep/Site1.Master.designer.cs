08940653623986ab22ed70cf174b41b426119e6d
MikiLand <74683061+MikiLand@users.noreply.github.com>
2023-06-14 22:18:42 +0200

    B7 - Solved, B9 - Attempt at moving to other page

--- Sklep/Site1.Master.designer.cs
+++ Sklep/Site1.Master.designer.cs
@@ -42,6 +42,15 @@ namespace Sklep
         protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
 
         /// <summary>
+        /// LinkButton2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton2;
+
+        /// <summary>
         /// NavBasket control.
         /// </summary>
         /// <remarks>
